e4c4aeb9c256592c493c3cf855af544013179d84
Liu <liujiajie.132@bytedance.com>
2019-03-12 14:38:25 +0800

    【完成JJDyFrameworkDemo】

---
+++ JJDyFrameworkDemo/JJDyFrameworkDemo/JJDyFrameworkDemo.h
@@ -0,0 +1,19 @@
+//
+//  JJDyFrameworkDemo.h
+//  JJDyFrameworkDemo
+//
+//  Created by Liu on 2019/3/12.
+//  Copyright © 2019年 Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJDyFrameworkDemo.
+FOUNDATION_EXPORT double JJDyFrameworkDemoVersionNumber;
+
+//! Project version string for JJDyFrameworkDemo.
+FOUNDATION_EXPORT const unsigned char JJDyFrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJDyFrameworkDemo/PublicHeader.h>
+
+
---
+++ JJDyFrameworkDemo/JJDyFrameworkDemo/JJDynamicLog.m
@@ -0,0 +1,17 @@
+//
+//  JJDynamicLog.m
+//  JJDyFrameworkDemo
+//
+//  Created by Liu on 2019/3/12.
+//  Copyright © 2019年 Liu. All rights reserved.
+//
+
+#import "JJDynamicLog.h"
+
+@implementation JJDynamicLog
+
+- (void)d_log:(NSString *)string {
+    NSLog(@"d_log = %@", string);
+}
+
+@end
