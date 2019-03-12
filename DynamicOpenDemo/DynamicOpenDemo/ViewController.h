b4ec3a977d8dc9373e517cc75f53f8ec12a29180
Liu <liujiajie.132@bytedance.com>
2019-03-12 14:28:35 +0800

    【创建DynamicOpenDemo】

---
+++ DynamicOpenDemo/DynamicOpenDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicOpenDemo
+//
+//  Created by Liu on 2019/3/12.
+//  Copyright © 2019年 Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DynamicOpenDemo/DynamicOpenDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DynamicOpenDemo
+//
+//  Created by Liu on 2019/3/12.
+//  Copyright © 2019年 Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
